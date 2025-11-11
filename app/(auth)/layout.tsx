a9b90e34efbfceab8e990a3df93569172458f8ae
davidjjeong <jaewoong.jeong@duke.edu>
2025-11-11 00:38:15 -0500

    Setup auth, added components and providers

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React, { ReactNode } from 'react'
 function layout({ children }: { children: ReactNode }) {
   return (
     <div className="relative flex h-screen w-full flex-col items-center justify-center">
-        <div className="mt-12">{children}</div>
+        <div className="">{children}</div>
     </div>
   )
 }
---
+++ components/Logo.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo() {
+  return (
+    <a href="" className="flex items-center gap-2"></a>
+  )
+}
+
+export default Logo
